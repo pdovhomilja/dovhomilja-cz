0c25d1e6ae8e698244d49797a5c50c61f073a907
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-05-08 22:09:54 +0200

    Update Next.js to 13.4 - because of App route is stable

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { withAxiom } = require("next-axiom");
 
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
-
 module.exports = withAxiom({
   ...nextConfig,
 });
